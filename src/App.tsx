7f5d89dfa4235d947d34212033e7a6952daeb962
Amo <emperoreddy@gmail.com>
2023-11-02 23:26:39 +0200

    error handling on router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import ChatLayout from "./components/ChatLayout";
 import background from "./assets/background.webp";
 import "./App.css";
 import UsernameSelect from "./components/UsernameSelect";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { createClient } from "@supabase/supabase-js";
 import NotFound from "./components/NotFound";
@@ -12,30 +13,42 @@ export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+const ErrorBoundaryLayout = () => (
+  <ErrorBoundary fallback={<div className="text-3xl text-white">Something went wrong 🥲</div>}>
+    <Outlet />
+  </ErrorBoundary>
+);
+
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Authentication/>,
+    element: <ErrorBoundaryLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Authentication />,
+      },
+      {
+        path: "/chat",
+        element: <ChatLayout />,
+      },
+      {
+        path: "/select-username",
+        element: <UsernameSelect />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
-  {
-    path: "/chat",
-    element: <ChatLayout />,
-  },
-  {
-    path: "/select-username",
-    element: <UsernameSelect />,
-  },
-  {
-    path: "*",
-    element: <NotFound/>,
-  }
 ]);
 
 function App() {
   return (
     <div
       className="bg-blue-800  w-screen h-screen flex items-center justify-center bg-cover bg-no-repeat"
-      style={{ backgroundImage: `url(${background})`, }}
+      style={{ backgroundImage: `url(${background})` }}
     >
       <RouterProvider router={router} />
     </div>
